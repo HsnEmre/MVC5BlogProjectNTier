164a4728ee476876353054337da84e64de750ecb
Hasan Emre Buzkan <hasanemre.b@hotmail.com>
2024-07-01 13:56:00 +0300

    Add project files.

---
+++ DataAccessLayer/Abstract/IRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IRepository<T>
+    {
+
+        #region Listele
+        List<T> List();
+        #endregion
+        #region ekle
+        int Insert(T p);
+        #endregion
+        #region Güncelle
+        int Update(T p);
+        #endregion
+        #region Sil
+        int Delete(T p);
+        #endregion
+
+        #region GetId
+        T GetByID(int id);
+        #endregion
+
+
+    }
+}
---
+++ EntityLayer/Concrete/Blog.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Blog
+    {
+        [Key]
+        public int BlogID { get; set; }
+        [StringLength(100)]
+        public string BlogTitle { get; set; }
+        [StringLength(200)]
+        public string BlogImage { get; set; }
+
+        public DateTime BlogDate { get; set; }
+        public string BlogContent { get; set; }
+
+        #region RelationCategory
+        public int CategoryID { get; set; }
+        public virtual Categories Categories { get; set; }
+        #endregion
+
+        #region RelationAuthor
+        public int AuthorID { get; set; }
+        public virtual Author Author { get; set; }
+        #endregion,
+
+
+        #region Relations Comments
+        public ICollection<Comments> Comments { get; set; }
+        #endregion
+    }
+}
